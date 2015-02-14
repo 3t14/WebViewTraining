5ee04deb701b9dedc5b3f96b380c49e3a5b7f079
Michiru Tanaka <michiru@r4hs.com>
2015-02-14 16:34:23 +0900

    Second Commit

---
+++ WebViewTraining/html/sample.js
@@ -0,0 +1,12 @@
+function addCalc(a, b) {
+    window.alert("addCalc");
+    window.alert("a + b = "+ ( a + b ));
+}
+
+addCalc(10, 20);
+
+
+//window.location = "native://?value1=hoge&value2=fuga";
+
+
+
